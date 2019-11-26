bdfe70058a55178ab8cec5654ce252b0db8ad37e
Geert Huls <geert.huls82@gmail.com>
2019-11-26 22:21:34 +0100

    Add author date of death column

---
+++ CourseLibrary.API/Migrations/AddDateOfDeathToAuthor.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseLibrary.API.Migrations
+{
+    public partial class AddDateOfDeathToAuthor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DateOfDeath",
+                table: "Authors",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfDeath",
+                table: "Authors");
+        }
+    }
+}
